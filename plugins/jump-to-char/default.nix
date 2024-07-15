43e098dba97b2ff68b70f4658a9bfdbcfdfe7a25
haennes <hannes.hofmuth@gmail.com>
2024-07-15 23:56:22 +0200

    jump-to-char

---
+++ plugins/jump-to-char/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation {
+  pname = "yaziPlugins-jump-to-char";
+  version = "2024-07-15";
+
+  src = fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "0dc9dcd5794ca7910043174ec2f2fe3561016983";
+    hash = "sha256-8RanvdS62IqkkKfswZUKynj34ckS9XzC8GYI9wkd3Ag=";
+  };
+
+  buildPhase = ''
+    mkdir $out
+    cp $src/jump-to-char.yazi/* $out
+  '';
+
+  meta = with lib; {
+    description = "Vim-like f<char>, jump to the next file whose name starts with <char>.";
+    homepage = "https://github.com/yazi-rs/plugins/tree/main/jump-to-char.yazi";
+    license = licenses.mit;
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
