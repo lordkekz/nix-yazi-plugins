924d6eddcf5ae35f748ae6b2c5dca9ded3b2636e
haennes <hannes.hofmuth@gmail.com>
2024-07-17 21:35:33 +0200

    full-border

---
+++ plugins/full-border/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation {
+  pname = "yaziPlugins-full-border";
+  version = "2024-07-15";
+
+  src = fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "c8b3e3979d79c110888d0bc2cab423f3d8093592";
+    hash = "sha256-x45hP/A6XtWloAjam71fC6wPgrv8kNTr/KDlJFGMz8Q=";
+  };
+
+  buildPhase = ''
+    mkdir $out
+    cp $src/full-border.yazi/* $out
+  '';
+
+  meta = with lib; {
+    description = "Add a full border to Yazi to make it look fancier.";
+    homepage = "https://github.com/yazi-rs/plugins/tree/main/full-border.yazi";
+    license = licenses.mit;
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
