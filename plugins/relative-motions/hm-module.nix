a77b917779b306ed3af40736aef67d5fb10273de
haennes <hannes.hofmuth@gmail.com>
2025-01-20 11:23:46 +0100

    relative-motions: add options

--- plugins/relative-motions/hm-module.nix
+++ plugins/relative-motions/hm-module.nix
@@ -23,15 +23,35 @@
           }
         ) 9
       );
+      show_numbers = lib.mkOption {
+        type = lib.types.enum [
+          "relative"
+          "absolute"
+          "relative_absolute"
+          null
+        ];
+        default = null;
+        description = "Shows relative or absolute numbers before the file icon";
+      };
+      show_motion = lib.mkEnableOption "Shows current motion in Status bar";
+      only_motions = lib.mkEnableOption ''
+        If true, only the motion movements will be enabled,
+        i.e., the commands for delete, cut, yank and visual selection will be disabled'';
     };
   config =
     { cfg, setKeys, ... }:
-    _:
-    {
-
-      programs.yazi.initLua = ''
-        require("starship"):setup()
-      '';
-    }
-    // (setKeys cfg.keys);
+    { lib, ... }:
+    lib.mkMerge [
+      (setKeys cfg.keys)
+      {
+        programs.yazi.initLua =
+          let
+            show_numbers =
+              if (cfg.show_numbers == null) then "" else ''show_numbers = "${cfg.show_numbers}", ''; # optinalString doesnt work here
+          in
+          ''
+            require("relative-motions"):setup({${show_numbers}show_motion = ${lib.trivial.boolToString cfg.show_motion}, only_motions=${lib.trivial.boolToString cfg.only_motions} })
+          '';
+      }
+    ];
 }
