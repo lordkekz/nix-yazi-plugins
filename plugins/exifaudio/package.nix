55b895f7cc97af76807309158e92381ed8374be8
haennes <hannes.hofmuth@gmail.com>
2025-02-28 22:07:08 +0100

    exifaudio to new api merged

--- plugins/exifaudio/package.nix
+++ plugins/exifaudio/package.nix
@@ -9,9 +9,9 @@ stdenv.mkDerivation {
   version = "unstable-2025-02-18";
 
   src = fetchFromGitHub {
-    owner = "haennes";
+    owner = "Sonico98";
     repo = "exifaudio.yazi";
-    rev = "6bc168bfe664c75cb943089f72b1b8cdf61b9e0b";
+    rev = "4379fcfa2dbe0b81fde2dd67b9ac2e0e48331419";
     hash = "sha256-CIimJU4KaKyaKBuiBvcRJUJqTG8pkGyytT6bPf/x8j8=";
   };
   buildPhase = ''
