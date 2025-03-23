1a859936f1d458636a6490f3cae2017d2a6c63ab
haennes <hannes.hofmuth@gmail.com>
2025-03-23 23:20:36 +0100

    change caseing

--- plugins/starship/hm-module.nix
+++ plugins/starship/hm-module.nix
@@ -2,13 +2,13 @@
   options =
     { mkDisableOption, ... }:
     _: {
-      enable_starship = mkDisableOption "wether to enable starship in home-manager";
+      enableStarship = mkDisableOption "wether to enable starship in home-manager";
     };
   config =
     { cfg, ... }:
     { lib, ... }:
     {
       programs.yazi.yaziPlugins.require.starship = { };
-      programs.starship.enable = lib.mkIf cfg.enable_starship true;
+      programs.starship.enable = lib.mkIf cfg.enableStarship true;
     };
 }
