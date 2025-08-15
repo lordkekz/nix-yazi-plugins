c7c1010a0283dcbcc7c70bbf2fdc588daaa9cf2c
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-15 16:02:44 -0400

    chore: update exifaudio package

--- plugins/exifaudio/package.nix
+++ plugins/exifaudio/package.nix
@@ -6,22 +6,23 @@
 
 stdenv.mkDerivation {
   pname = "yaziPlugins-exifaudio";
-  version = "unstable-2025-02-18";
+  version = "unstable-2025-06-20";
 
   src = fetchFromGitHub {
     owner = "Sonico98";
     repo = "exifaudio.yazi";
-    rev = "4379fcfa2dbe0b81fde2dd67b9ac2e0e48331419";
-    hash = "sha256-CIimJU4KaKyaKBuiBvcRJUJqTG8pkGyytT6bPf/x8j8=";
+    rev = "4506f9d5032e714c0689be09d566dd877b9d464e";
+    hash = "sha256-RWCqWBpbmU3sh/A+LBJPXL/AY292blKb/zZXGvIA5/o=";
   };
+
   buildPhase = ''
     mkdir $out
     cp $src/* $out
   '';
 
   meta = with lib; {
-    description = "Preview audio files metadata on yazi ";
-    homepage = "Preview audio files metadata on yazi ";
+    description = "Preview audio files metadata on yazi";
+    homepage = "https://github.com/Sonico98/exifaudio.yazi";
     license = licenses.mit;
     maintainers = [ ];
     platforms = platforms.all;
