c6adcc87c1f6a9944ffea4c3ee964bd9695d30a6
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-21 19:54:13 -0400

    feat: add theme support and yatline-catppuccin theme to yatline

--- plugins/yatline/hm-module.nix
+++ plugins/yatline/hm-module.nix
@@ -3,6 +3,32 @@
     _:
     { lib, ... }:
     {
+      theme = {
+        name = lib.mkOption {
+          type =
+            with lib.types;
+            nullOr (enum [
+              "catppuccin"
+            ]);
+          description = ''
+            The name of the theme to set.
+            Note that this is limited to the yatline theme plugins currently packaged.
+          '';
+          example = "catppuccin";
+          default = null;
+        };
+
+        setup = lib.mkOption {
+          type = with lib.types; either str attrs;
+          description = ''
+            Options to pass to theme plugin when running `setup` on it.
+            Note that this is highly specific to the theme in question.
+          '';
+          example = "mocha";
+          default = { };
+        };
+      };
+
       # NOTE: yatline has a lot of configuration options
       extraSetup = lib.mkOption {
         type = with lib.types; attrs;
@@ -27,8 +53,31 @@
     };
   config =
     { cfg, ... }:
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
+    let
+      luaFormat = lib.generators.toLua { };
+      requirePlugin = name: setup: ''
+        require("${name}"):setup(${if setup != { } then luaFormat setup else ""})
+      '';
+      themeIsSet = !isNull cfg.theme.name;
+      themeName = "yatline-${cfg.theme.name}";
+    in
     {
-      programs.yazi.yaziPlugins.require."yatline" = cfg.extraSetup;
+      programs.yazi = {
+        plugins = lib.mkIf themeIsSet {
+          ${themeName} = pkgs.yaziPlugins.${themeName};
+        };
+
+        yaziPlugins = {
+          preRequire."yatline" = lib.mkIf themeIsSet ''
+            local yatline_theme = ${requirePlugin themeName cfg.theme.setup}
+          '';
+          require."yatline" =
+            cfg.extraSetup
+            // (lib.optionalAttrs themeIsSet {
+              theme = if themeIsSet then (lib.generators.mkLuaInline "yatline_theme") else null;
+            });
+        };
+      };
     };
 }
